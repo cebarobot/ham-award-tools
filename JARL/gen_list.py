93e4851f1a1a288a3b08b7fd12a55b108f160080
cebarobot <ceba_robot@outlook.com>
2022-10-10 23:39:20 +0800

    feat: merge pref/jcc/jcg/ku list

--- JARL/gen_list.py
+++ JARL/gen_list.py
@@ -1,9 +1,8 @@
 import re
 import json
 
-jcc_list = {}
-jcg_list = {}
-ku_list = {}
+with open("pref-list.json", "r", encoding="utf-8") as f:
+    no_list = json.load(f)
 
 # deal with JCC list
 with open("jcc-list.txt", "r", encoding='shift_jis') as f:
@@ -20,18 +19,20 @@ with open("jcc-list.txt", "r", encoding='shift_jis') as f:
         else:
             res = m.match(line)
             if res and res.group(1) == ' ':
-                this_jcc = res.group(2)
+                this_no = res.group(2)
                 this_romaji = res.group(3)
                 this_kanji = res.group(4)
-                if this_jcc == '1001':
+                if this_no == '1001':
                     # Tokyo 23 wards
-                    jcc_list[this_jcc] = {
+                    no_list[this_no] = {
+                        "type": "City",
                         "romaji": this_romaji + '-Ku',
                         "kanji": this_kanji + '区'
                     }
                 else:
                     # other cities
-                    jcc_list[this_jcc] = {
+                    no_list[this_no] = {
+                        "type": "City",
                         "romaji": this_romaji + '-Shi',
                         "kanji": this_kanji + '市'
                     }
@@ -52,23 +53,26 @@ with open("jcg-list.txt", "r", encoding='shift_jis') as f:
         else:
             res = m.match(line)
             if res and res.group(1) == ' ':
-                this_jcg = res.group(2)
+                this_no = res.group(2)
                 this_romaji = res.group(3)
                 this_kanji = res.group(4)
                 if this_romaji.count("("):
                     romaji_i = this_romaji.index("(")
                     kanji_i = this_kanji.index("(")
-                    jcg_list[this_jcg] = {
+                    no_list[this_no] = {
+                        "type": "Gun",
                         "romaji": this_romaji[0:romaji_i] + '-gun' + this_romaji[romaji_i:],
                         "kanji": this_kanji[0:kanji_i] + '郡' + this_kanji[kanji_i:]
                     }
                 elif this_romaji.count("-Shicho"):
-                    jcg_list[this_jcg] = {
+                    no_list[this_no] = {
+                        "type": "Gun",
                         "romaji": this_romaji,
                         "kanji": this_kanji
                     }
                 else:
-                    jcg_list[this_jcg] = {
+                    no_list[this_no] = {
+                        "type": "Gun",
                         "romaji": this_romaji + '-Gun',
                         "kanji": this_kanji + '郡'
                     }
@@ -89,15 +93,12 @@ with open("ku-list.txt", "r", encoding='shift_jis') as f:
         else:
             res = m.match(line)
             if res:
-                ku_list[res.group(1)] = {
+                no_list[res.group(1)] = {
+                    "type": "Ku",
                     "romaji": res.group(2) + '-Ku',
                     "kanji": res.group(3) + '区'
                 }
 
 
-with open("jcg-list.json", "w", encoding='utf-8') as f:
-    json.dump(jcg_list, f, ensure_ascii=False, indent=4)
-with open("jcc-list.json", "w", encoding='utf-8') as f:
-    json.dump(jcc_list, f, ensure_ascii=False, indent=4)
-with open("ku-list.json", "w", encoding='utf-8') as f:
-    json.dump(ku_list, f, ensure_ascii=False, indent=4)
+with open("no-list.json", "w", encoding='utf-8') as f:
+    json.dump(no_list, f, ensure_ascii=False, indent=4)
