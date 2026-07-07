1fe8674e666646681ed4b90faaa1c74f040adf41
ceba <ceba_robot@outlook.com>
2026-07-07 17:26:52 +0800

    build: add workflow to deploy pages
    Signed-off-by: ceba <ceba_robot@outlook.com>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ui from '@nuxt/ui/vite'
 
+const base = process.env.VITE_BASE_PATH || '/'
+
 export default defineConfig({
+  base,
   plugins: [vue(), ui()],
   resolve: {
     alias: {
